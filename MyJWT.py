90c3c62b64b8230d58f349c4b2fd0f8daaf9c2ba
1556855706@qq.com <1556855706@qq.com>
2020-05-31 08:29:53 +0800

    基于JWT规范自定义的JWT并优化

---
+++ MyJWT.py
@@ -0,0 +1,78 @@
+import base64
+import json
+import time
+import copy
+import hmac
+
+
+class Jwt():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def b64encode(content):
+        return base64.urlsafe_b64encode(content).replace(b'=', b'')
+
+    @staticmethod
+    def b64decode(b):
+        # 如何将去掉的=号加回来,base64串有某个规律可以实现该需求
+        # base64长度能被4整除
+
+        sem = len(b) % 4
+        if sem > 0:
+            b += b'=' * (4 - sem)
+        return base64.urlsafe_b64decode(b)
+
+    @staticmethod
+    def encode(payload, key, exp=300):
+
+        # init header
+        header = {'typ': 'JWT', 'alg': 'HS256'}
+        header_json = json.dumps(header, separators=(',', ':'), sort_keys=True)
+        header_bs = Jwt.b64encode(header_json.encode())
+
+        # init payload
+        payload_self = copy.deepcopy(payload)
+        if not isinstance(exp, int) and not isinstance(exp, str):
+            raise TypeError('exp must be int or str !')
+        payload_self['exp'] = time.time() + int(exp)
+        payload_js = json.dumps(payload_self, separators=(',', ':'), sort_keys=True)
+        payload_bs = Jwt.b64encode(payload_js.encode())
+
+        # init sign
+        if isinstance(key, str):
+            key = key.encode()
+        hm = hmac.new(key, header_bs + b'.' + payload_bs, digestmod='SHA256')
+        sign_bs = Jwt.b64encode(hm.digest())
+
+        return header_bs + b'.' + payload_bs + b'.' + sign_bs
+
+    @staticmethod
+    def decode(token, key):
+        # 校验签名
+        # 检查exp是否过期
+        # return payload部分的解码
+        header_bs, payload_bs, sign_bs = token.split(b'.')
+        # 校验sign_bs
+        if isinstance(key, str):
+            key = key.encode()
+            hm  = hmac.new(key, header_bs + b'.' + payload_bs, digestmod='SHA256')
+            # 对比两次的sign结果
+            if sign_bs != Jwt.b64encode(hm.digest()):
+                raise
+            # 检查是否过期
+            payload_js = Jwt.b64decode(payload_bs)
+            payload = json.loads(payload_js)
+
+            if 'exp' in payload:
+                now = time.time()
+                if now > payload['exp']:
+                    raise
+            return payload
+
+if __name__ == '__main__':
+    token = Jwt.encode({'username': 'guoxiaonao'}, '123456', 300)
+    print('生成token如下:')
+    print(token)
+    print('校验结果如下：')
+    print(Jwt.decode(token, '123456'))
